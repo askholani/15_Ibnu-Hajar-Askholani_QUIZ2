93740233a1cc495341c22a3c0f9fe243006252df
askholani <askholani210@gmail.com>
2022-06-04 14:02:08 +0700

    kuis2

--- DLLMain.java
+++ DLLMain.java
@@ -8,9 +8,9 @@ public class DLLMain {
       System.out.println("4. Laporan Pengurutan pesanan");
       System.out.println("5. Hitung total pendapatan");
       System.out.println("6. Keluar");
+      System.out.print("pilih (1-5) : ");
    }
    public static void main(String[] args) {
-      Scanner s = new Scanner(System.in);
       System.out.println("==================== Quiz 2 Praktikum ASD TI - 1F ====================");
       String name = "Ibnu Hajar Askholani";
       String nim = "2141720170";
@@ -19,41 +19,121 @@ public class DLLMain {
       System.out.println("NIM : " + nim);
       System.out.println("Absen : " + absen);
 
-      String np1 = "Mamluatul";
-      String np11 = "082245000000";
-      String np2 = "Abyaz A.M";
-      String np22 = "082245111111";
-      String np3 = "Yoshinoya";
-      String np33 = "082245222222";
-      String np4 = "Susi Susanti";
-      String np44 = "081234456";
-
-      String nps1 = "Bugologi";
-      int nps11 = 25000;
-      String nps2 = "Mie Goreng";
-      int nps22 = 15000;
-      String nps3 = "Nasi Rendang";
-      int nps33 = 25000;
-      String nps4 = "Soto Ayam";
-      int nps44 = 15000;
-
+      Scanner s = new Scanner(System.in);
       DoubleLinkedList m = new DoubleLinkedList();
-      Pembeli_11 pb1 = new Pembeli_11(np1, np11);
-      Pembeli_11 pb2 = new Pembeli_11(np2, np22);
-      Pembeli_11 pb3 = new Pembeli_11(np3, np33);
-      Pembeli_11 pb4 = new Pembeli_11(np4, np44);
+      boolean again = true;
+      menu();
+      int pilih = s.nextInt();
+      while (again) {
+         while (again) {
+            if (pilih == 1) {
+               System.out.println("=============================");
+               System.out.println("Masukkan Data Pembeli");
+               System.out.println("=============================");
 
-      Pesanan_11 ps1 = new Pesanan_11(1, nps1, nps11);
-      Pesanan_11 ps2 = new Pesanan_11(2, nps2, nps22);
-      Pesanan_11 ps3 = new Pesanan_11(3, nps3, nps33);
-      Pesanan_11 ps4 = new Pesanan_11(4, nps4, nps44);
+               String np1 = "Mamluatul";
+               String np11 = "082245000000";
+               String np2 = "Abyaz A.M";
+               String np22 = "082245111111";
+               String np3 = "Yoshinoya";
+               String np33 = "082245222222";
+               String np4 = "Susi Susanti";
+               String np44 = "081234456";
 
-      m.add(pb1, ps1);
-      m.add(pb2, ps2);
-      m.add(pb3, ps3);
-      m.add(pb4, ps4);
+            Pembeli_11 pb1 = new Pembeli_11(np1, np11);
+            Pembeli_11 pb2 = new Pembeli_11(np2, np22);
+            Pembeli_11 pb3 = new Pembeli_11(np3, np33);
+            Pembeli_11 pb4 = new Pembeli_11(np4, np44);
 
-      m.cetak();
+            m.addPb(1, pb1);
+            m.addPb(2, pb2);
+            m.addPb(3, pb3);
+            m.addPb(4, pb4);
 
+               menu();
+            }  else if (pilih == 2) {
+               System.out.println("+++++++++++++++++++++++++++++++");
+               System.out.println("Daftar Antrian Resto Royal Delish");
+               System.out.println("+++++++++++++++++++++++++++++++");
+               m.cetak();
+
+               String nps1 = "Bugologi";
+               int nps11 = 25000;
+               String nps2 = "Mie Goreng";
+               int nps22 = 15000;
+               String nps3 = "Nasi Rendang";
+               int nps33 = 25000;
+               String nps4 = "Soto Ayam";
+               int nps44 = 15000;
+
+               Pesanan_11 ps1 = new Pesanan_11(1, nps1, nps11);
+               Pesanan_11 ps2 = new Pesanan_11(2, nps2, nps22);
+               Pesanan_11 ps3 = new Pesanan_11(3, nps3, nps33);
+               Pesanan_11 ps4 = new Pesanan_11(4, nps4, nps44);      
+               
+               m.addPs(1, ps1);
+               m.addPs(2, ps2);
+               m.addPs(3, ps3);
+               m.addPs(4, ps4);
+               menu();
+            } else if (pilih == 3) {
+               m.hapus();
+               menu();
+            } else if (pilih  == 4) {
+               m.cetak1();
+               menu();
+            } else if (pilih == 5) {
+               System.out.println(m.hitung());
+               menu();
+            } else {
+               again =false;
+               break;
+            }
+            pilih = s.nextInt();
+         }
+      }
+
+
+      // System.out.println("==================== Quiz 2 Praktikum ASD TI - 1F ====================");
+      // String name = "Ibnu Hajar Askholani";
+      // String nim = "2141720170";
+      // String absen = "11 / 15";
+      // System.out.println("dibuat oleh : " + name);
+      // System.out.println("NIM : " + nim);
+      // System.out.println("Absen : " + absen);
+
+      // String np1 = "Mamluatul";
+      // String np11 = "082245000000";
+      // String np2 = "Abyaz A.M";
+      // String np22 = "082245111111";
+      // String np3 = "Yoshinoya";
+      // String np33 = "082245222222";
+      // String np4 = "Susi Susanti";
+      // String np44 = "081234456";
+
+      // String nps1 = "Bugologi";
+      // int nps11 = 25000;
+      // String nps2 = "Mie Goreng";
+      // int nps22 = 15000;
+      // String nps3 = "Nasi Rendang";
+      // int nps33 = 25000;
+      // String nps4 = "Soto Ayam";
+      // int nps44 = 15000;
+
+      // DoubleLinkedList m = new DoubleLinkedList();
+      // Pembeli_11 pb1 = new Pembeli_11(np1, np11);
+      // Pembeli_11 pb2 = new Pembeli_11(np2, np22);
+      // Pembeli_11 pb3 = new Pembeli_11(np3, np33);
+      // Pembeli_11 pb4 = new Pembeli_11(np4, np44);
+
+      // Pesanan_11 ps1 = new Pesanan_11(1, nps1, nps11);
+      // Pesanan_11 ps2 = new Pesanan_11(2, nps2, nps22);
+      // Pesanan_11 ps3 = new Pesanan_11(3, nps3, nps33);
+      // Pesanan_11 ps4 = new Pesanan_11(4, nps4, nps44);      
+      
+      // m.addPs(1, ps1);
+      // m.addPs(2, ps2);
+      // m.addPs(3, ps3);
+      // m.addPs(4, ps4);
    }
 }
--- DoubleLinkedList.java
+++ DoubleLinkedList.java
@@ -1,3 +1,4 @@
+import java.rmi.Remote;
 import java.util.Arrays;
 public class DoubleLinkedList {
    Node headPb;
@@ -10,25 +11,74 @@ public class DoubleLinkedList {
       size = 0;
    }
 
-   public boolean isEmpty () {
-      return headPs == null && headPb == null;
+   public void addFirstPb (Pembeli_11 pb) {
+      if (headPb == null ) {
+         headPb = new Node (null ,pb ,null);
+      } else {
+         Node newNodepb = new Node(null, pb, headPb);
+         headPb.next =newNodepb;
+      }
    }
 
-   public void add (Pembeli_11 pb ,Pesanan_11 ps) {
-      if (isEmpty() ) {
-         headPb = new Node (null ,pb ,null);
-         headPs = new Node (null ,pb ,null);
+   public void addFirstPs (Pesanan_11 ps) {
+      if (headPs == null ) {
+         headPs = new Node(null ,ps ,null);
       } else {
-         Node newNodePb = new Node (headPb , pb ,null);
-         Node newNodePs = new Node (headPs , ps ,null);
-         headPb.prev = newNodePb;
-         headPs.prev = newNodePs;
+         Node newNodePs = new Node(null, ps, null);
+         headPs.next = newNodePs;
+      }
+   }
+
+   
+   public void addPb (int urutan ,Pembeli_11 pb ) throws ArrayIndexOutOfBoundsException {
+      if (headPb == null ) {
+         addFirstPb(pb);
+      } else if (urutan - 1 < 0 || urutan-1 > size) {
+         throw new ArrayIndexOutOfBoundsException("NIlai urutan di luar batas");
+      } else {
+         Node currentPb = headPb;
+         int i = 1;
+         while (i < urutan-1) {
+            currentPb = currentPb.next;
+            i++;
+         }
+         if (currentPb.prev == null) {
+            Node newNodePb = new Node(currentPb , pb, null);
+            currentPb.next = newNodePb;
+         } else {
+            Node newNodePb = new Node(currentPb, pb, null);
+            currentPb.next = newNodePb;
+         }
       }
       size++;
    }
 
+   public void addPs (int urutan ,Pesanan_11 ps ) throws ArrayIndexOutOfBoundsException {
+      if (headPs == null ) {
+         addFirstPs(ps);
+      } else if (urutan - 1 < 0 ) {
+         throw new ArrayIndexOutOfBoundsException("nomer pesanan di luar batas");
+      } else {
+         Node currentPs = headPs;
+         int i = 1;
+         while (i < urutan-1) {
+            currentPs = currentPs.next;
+            i++;
+         }
+         if (currentPs.prev == null) {
+            Node newNodePs = new Node(currentPs , ps, null);
+            currentPs.next = newNodePs;
+         } else {
+            Node newNodePs = new Node(currentPs, ps, null);
+            currentPs.next = newNodePs;
+         }
+      }
+   }
+
+
+
    public void cetak () {
-      if (!isEmpty() ) {
+      if (!(headPb == null) ) {
          Node tmp = headPb;
          int numb = 1;
          while (tmp != null) {
@@ -39,33 +89,40 @@ public class DoubleLinkedList {
       }
    }
 
-   public void hapus () throws ArrayIndexOutOfBoundsException {
-      if (isEmpty() ) {
-         throw new ArrayIndexOutOfBoundsException("Antrian masih kosong");
-      } else {
-         headPb = headPb.next;
-         headPb.prev = null;
-         size--;
+   public void cetak1 () {
+      if(!(headPs == null)) {
+         Node tmp = headPs;
+         while(tmp != null) {
+            System.out.println(tmp.pesanan.kodePesanan + "   " + tmp.pesanan.namaPesanan + "   " + tmp.pesanan.harga);
+            tmp = tmp.next;
+         }
       }
    }
 
-   // public void sortPesananByName () {
-   //    String [] sortPs;
-   //    sortPs[size];
-   //    Node current = headPs;
-   //    for (int i = 0 ; i < size ; i++) {
-   //       sortPs[i] = current.pesanan.namaPesanan;
-   //       current = current.next;
-   //    }
-   //    Arrays.sort(sortPs);
+   public void hapus () throws ArrayIndexOutOfBoundsException {
+      if (headPb == null) {
+         throw new ArrayIndexOutOfBoundsException("antrian masih kosong");
+      } else {
+         System.out.println(headPb.pembeli.namaPembeli + " telah memesan menu");
+         System.out.println("+++++++++++++++++++++++++++");
+         System.out.println("Transaksi Input Pesanan");
+         System.out.println("+++++++++++++++++++++++++++");
+         System.out.println("Nomor Pesanan : " + headPs.pesanan.kodePesanan);
+         System.out.println("Pesanan : " + headPs.pesanan.namaPesanan);
+         System.out.println("Harga : " + headPs.pesanan.harga);
+         headPb = headPb.next;
+         headPb.prev = null;
+         headPs = headPs.next;
+         headPs.prev = null;
+      }
+   }
 
 
-   // }
 
    public int hitung () {
       int total = 0;
       Node current = headPs;
-      while(current.next != null) {
+      while(current != null) {
          total += current.pesanan.harga;
          current = current.next;
       }
